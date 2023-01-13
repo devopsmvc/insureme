fed8806b78d58edebb6ce295b7273d5e2d3534a4
devopsmvc <devops.mvc@gmail.com>
2023-01-13 12:52:25 +0530

    updated ansible and Junit Test file

--- src/test/java/com/hdfc/insureme/InsuremeApplicationTests.java
+++ src/test/java/com/hdfc/insureme/InsuremeApplicationTests.java
@@ -6,7 +6,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import io.restassured.RestAssured;
-import io.restassured.http.ContentType;
 import io.restassured.http.Method;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
