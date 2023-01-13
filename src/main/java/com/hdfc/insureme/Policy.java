a0c2578f3a7430861c50a589ef12963d7e681eff
devopsmvc <devops.mvc@gmail.com>
2023-01-13 13:02:50 +0530

    updated policy file

--- src/main/java/com/hdfc/insureme/Policy.java
+++ src/main/java/com/hdfc/insureme/Policy.java
@@ -9,7 +9,7 @@ import javax.persistence.Id;
 public class Policy {
 	
 	@Id
-	//@GeneratedValue(strategy = GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private int PolicyID;	
 	private String PolicyName;	
 	private double PolicyNumber;
